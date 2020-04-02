29bc3b68d430f106631a3825bb9aca3ff17857d2
albidema <albidema@live.com>
2020-04-02 15:20:50 +0200

    End Of Section 9

---
+++ DatingApp-SPA/src/app/_models/photo.ts
@@ -0,0 +1,7 @@
+export interface photo {
+    id: number;
+    url: string;
+    description: string;
+    dataAdded: Date;
+    isMain: boolean;
+}
---
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -0,0 +1,18 @@
+import { photo } from './photo';
+
+export interface User {
+    id: number;
+    username: string;
+    knownAs: string;
+    age: number;
+    gender: number;
+    created: Date;
+    lastActive: Date;
+    photoUrl: string;
+    city: string;
+    country: string;
+    interests?: string;
+    introduction?: string;
+    lookingFor?: string;
+    photos?: photo[];
+}
